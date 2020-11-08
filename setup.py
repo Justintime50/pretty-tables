1fcff98d6f1735fcdb051d639171ce922c93b750
Justintime50 <justinpaulhammond@gmail.com>
2020-11-07 18:46:47 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pretty-tables',
+    version='1.0.0',
+    description='Create pretty tables from headers and rows, perfect for console output.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/pretty-tables',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        'dev': [
+            'pytest >= 6.0.0',
+            'pytest-cov >= 2.10.0',
+            'coveralls >= 2.1.2',
+            'flake8 >= 3.8.0',
+            'mock >= 4.0.0',
+        ]
+    },
+    python_requires='>=3.6',
+)
